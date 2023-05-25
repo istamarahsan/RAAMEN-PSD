8ead15b87a632f600cdc6cbdec140ad4e2473f33
San <istamarahsan@gmail.com>
2023-05-25 15:49:40 +0700

    sql server strings (perhaps)

--- PSD-Project/API/Util/Sql/QueryStrings/SqlServerQueryStringBuilder.cs
+++ PSD-Project/API/Util/Sql/QueryStrings/SqlServerQueryStringBuilder.cs
@@ -6,11 +6,15 @@ namespace PSD_Project.API.Util.Sql.QueryStrings
     {
         public override string StringForAddHeader(int headerId, int customerId, int staffId, DateTime date)
         {
+            // maybe this??
+            // return $"INSERT INTO [Header]([id], [CustomerId], [StaffId], [Date]) VALUES ({headerId}, {customerId}, {staffId}, convert(DATE, '{date.Year:0000}-{date.Month:00}-{date.Day:00}', 23))";
             throw new NotImplementedException();
         }
 
         public override string StringForAddDetails(int headerId, int ramenId, int quantity)
         {
+            // maybe
+            // return $"insert into [Detail]([Headerid], [Ramenid], [Quantity]) VALUES ({headerId}, {ramenId}, {quantity})";
             throw new NotImplementedException();
         }
     }
